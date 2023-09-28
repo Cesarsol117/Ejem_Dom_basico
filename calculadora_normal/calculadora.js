d6fe5b4f69ccf354716a8568a941c91882b2c9c2
Cesarsol117 <cesar_p117@hotmail.com>
2023-09-28 08:30:42 -0500

    Primera parte de triangulo isoceles

--- calculadora_normal/calculadora.js
+++ calculadora_normal/calculadora.js
@@ -1,19 +1,105 @@
 // imputs
 const operador1 = document.querySelector('#operator1');
 const operador2 = document.querySelector('#operator2');
+// imputs triangulos
+const alturaImputIsoceles = document.querySelector('#isoceles-triangle-heigth-imput');
+const baseImputIsoceles = document.querySelector('.isoceles-triangle-base-imput');
+const aLadoImputIsoceles = document.querySelector('#isoceles-triangle-sideA-imput');
+
 // botones
 const botonPlus = document.querySelector('#sum');
 const botonRest = document.querySelector('#rest');
 const botonMult = document.querySelector('#mult');
 const botonDivi = document.querySelector('#divi');
 const botonEqual = document.querySelector('#equalBotton');
-// vista
+// tringulos
+const resultAreaIndicator = document.querySelector('.isoceles-triangle-indicator');
+
+// vista de resultados de operaciones
 const resultadOper = document.querySelector('#result');
+const resultAreaTriangleIsoceles = document.querySelector('#area-result-triangle-isoceles');
+const resultAreaTriangleIsoceleSides = document.querySelector('#area-result-triangle-isoceles-sides');
+
+
+// vistas y secciones
+
+const viewCalcMain = document.querySelector('.mainBoard');
+const viewChoiceTriangle = document.querySelector('.trianhgulo-section');
+const trianguloIsocelesoperaciones = document.querySelector('.triangles-view');
+
+// div s de cada clase
+const linkMoreProject = document.querySelector('#show-sid-bar');
+const sideBar = document.querySelector('.side-bar');
+// ---------- menu mobile -----------
+const mobileBotonMenu = document.querySelector('.navbar-hamburger');
+const menuMobile = document.querySelector('.mobile-menu');
+// links para camviar de vista
+const linkInicioPrincipal = document.querySelector('.inicio-principal');
+const linkTriangleChoice =  document.querySelector('#triangle-link');
+const linkTrianguloIsoceles = document.querySelector('.choice-triangle-isoceles');
+
+
+
+
 // funciones de operacion
 botonPlus.addEventListener('click', sumarInputs); 
 botonRest.addEventListener('click', restInputs); 
 botonMult.addEventListener('click', multInputs); 
-botonDivi.addEventListener('click', divInputs); 
+botonDivi.addEventListener('click', divInputs);
+// eventos del nav
+linkInicioPrincipal.addEventListener('click', vistaInicioPaginaPrincipal);
+linkMoreProject.addEventListener('mouseover', mouseoverSideBar);
+mobileBotonMenu.addEventListener('click', showMobileMenu);
+// eventos de los link de seleccion de figuras geometricas
+linkTriangleChoice.addEventListener('click', viewTriangleCalc);
+linkTrianguloIsoceles.addEventListener('click', vistaOperacionesTrianguloIsoceles)
+// tribgulo isoceles
+resultAreaIndicator.addEventListener('click', calcAreaTrianguloIsoceles);
+resultAreaIndicator.addEventListener('click', calcAreaTrianguloIsocelesSides);
+
+// funciones para nav bar
+function mouseoverSideBar(){
+    sideBar.classList.toggle('inactive');
+    console.log('voy');
+}
+function showMobileMenu(){
+    menuMobile.classList.toggle('inactivem');
+    console.log('soy')
+}
+// vitas inicio principal
+function vistaInicioPaginaPrincipal(){
+    viewCalcMain.classList.remove('inactive');
+    viewChoiceTriangle.classList.add('inactive')
+    trianguloIsocelesoperaciones.classList.add('inactive')
+}
+
+// funciones para vistas de triangulos
+function viewTriangleCalc(){
+    viewCalcMain.classList.add('inactive');
+    viewChoiceTriangle.classList.remove('inactive');
+    trianguloIsocelesoperaciones.classList.add('inactive')
+    
+}
+function vistaOperacionesTrianguloIsoceles(){
+    viewChoiceTriangle.classList.add('inactive')
+    trianguloIsocelesoperaciones.classList.remove('inactive')
+
+}
+
+
+
+// funcion de calculo de triangulos
+function calcAreaTrianguloIsoceles(){
+    const areaIsocelesBasic = (parseInt(baseImputIsoceles.value) * parseInt(alturaImputIsoceles.value))/2;
+    resultAreaTriangleIsoceles.innerText = areaIsocelesBasic;
+}
+function calcAreaTrianguloIsocelesSides(){
+    const alturaTriangulo = Math.sqrt(((parseFloat(aLadoImputIsoceles.value)) ** 2 ) - (((parseFloat(baseImputIsoceles.value)) ** 2)/4));
+    console.log(alturaTriangulo);
+    const areaIsocelesSide = (parseFloat(baseImputIsoceles.value)*alturaTriangulo)/2;
+    resultAreaTriangleIsoceleSides.innerText = areaIsocelesSide + " unidades cuadradas";
+}
+
 
 function sumarInputs(){
     const resSuma = parseInt(operador1.value) +parseInt(operador2.value);
